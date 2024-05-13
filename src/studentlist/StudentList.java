fcd45385c03f8699e87ff43b923f0689911bd391
Nupur Dhiman <dhimannu@sheridancollege.ca>
2024-05-13 14:15:20 -0400

    initial

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,40 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author nupur
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] studentList = new Student[3];
+        Student s1 = new Student();
+        s1.setName("Nupur");
+        Student s2 = new Student();
+        s2.setName("Saurav");
+        Student s3 = new Student();
+        s3.setName("Sahil");
+        Student s4 = new Student();
+        s4.setName("Arjun");
+        studentList[0] = s1;
+        studentList[1] = s2;
+        studentList[2] = s3;
+        studentList[3] = s4;
+        //this loop is going to run the number of times 
+        //as same as the elements in the index.
+        for(Student s: studentList){
+            System.out.println(s.getName());
+        }
+        
+            
+    }
+    
+}
